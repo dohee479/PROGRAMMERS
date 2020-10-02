e260b0e224f8254c3be49ef192c10bd9617c4b80
dohee479 <dohee479@naver.com>
2020-10-03 00:22:14 +0900

    프로그래머스

--- Level 1/정수 내림차순으로 배치하기.py
+++ Level 1/정수 내림차순으로 배치하기.py
@@ -1,6 +1,8 @@
 def solution(n):
     answer = 0
-
+    number = sorted(str(n), reverse=True)
+    for index, num in enumerate(number):
+        answer += int(num) * (10 ** (len(number) - index  - 1 ))
     return answer
 
 print(solution(118372))
