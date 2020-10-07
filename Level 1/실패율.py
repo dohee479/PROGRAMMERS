2f6973ac9e2c6b552f1517dab25012bea7007f13
dohee479 <dohee479@naver.com>
2020-10-07 14:08:06 +0900

    프로그래머스

--- Level 1/실패율.py
+++ Level 1/실패율.py
@@ -1,15 +1,19 @@
 def solution(N, stages):
     gamer = len(stages)
     count = [0] * (N + 1)
-    fail = [0] * (N + 1)
+    fail = {}
+    pre_value = 0
     for i in stages:
-        if i <= N:
+        if i < N+1:
             count[i] += 1
     for i, value in enumerate(count):
-        if i and value:
-            gamer -= count[i - 1]
-            fail[i] = value / gamer
-    answer = sorted(range(1, N+1), key=lambda x: fail[x], reverse=True)
-    return answer
+        if i:
+            if gamer and value:
+                gamer -= pre_value
+                fail[i] = value / gamer
+                pre_value = value
+            else:
+                fail[i] = 0
+    return sorted(fail, key=lambda x : fail[x], reverse=True)
 
-print(solution(8, [1,2,3,4,5,6,7]))
+print(solution(5, [2, 1, 2, 6, 2, 4, 3, 3]))
