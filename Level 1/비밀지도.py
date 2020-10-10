f4ab952a1b77ab9466092c4234e9cb395e291b8d
dohee479 <dohee479@naver.com>
2020-10-10 22:25:47 +0900

    프로그래머스

--- Level 1/비밀지도.py
+++ Level 1/비밀지도.py
@@ -1,20 +1,17 @@
-def maps(arr):
+def maps(arr, n):
     global secret
-    for i, value in enumerate(arr):
-        for j, b in enumerate(bin(value)):
-            if j > 1:
-                if b == '1':
-                    secret[i][j - 1] = '#'
-                else:
-                    secret[i][j - 1] = ' '
+    for i in arr:
+        for j, value in enumerate('0' * (n - len(bin(i)[2:]) + str(bin(i)))):
+            print(value, end=" ")
+        print()
+
 
 def solution(n, arr1, arr2):
     answer = [''] * n
     global secret
     secret = [[''] * n for _ in range(n)]
-    maps(arr1)
-    maps(arr2)
-    print(secret)
+    maps(arr1, n)
+    maps(arr2, n)
     # for i, value in enumerate(secret):
     #     for j in value:
     #         answer[i] += j
