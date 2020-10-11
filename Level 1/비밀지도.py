8ccf48664b79eb15d62d772e39bbc5a0e12f1c51
dohee479 <dohee479@naver.com>
2020-10-11 18:38:48 +0900

    프로그래머스

--- Level 1/비밀지도.py
+++ Level 1/비밀지도.py
@@ -1,9 +1,13 @@
 def maps(arr, n):
     global secret
-    for i in arr:
-        for j, value in enumerate('0' * (n - len(bin(i)[2:]) + str(bin(i)))):
-            print(value, end=" ")
-        print()
+    for i, num in enumerate(arr):
+        for j, value in enumerate(('0' * (n - len(bin(num)[2:])) + bin(num)[2:])):
+            if value == "0":
+                if secret[i][j] == "#":
+                    continue
+                secret[i][j] = ' '
+            else:
+                secret[i][j] = '#'
 
 
 def solution(n, arr1, arr2):
@@ -12,10 +16,20 @@ def solution(n, arr1, arr2):
     secret = [[''] * n for _ in range(n)]
     maps(arr1, n)
     maps(arr2, n)
-    # for i, value in enumerate(secret):
-    #     for j in value:
-    #         answer[i] += j
+    for i, value in enumerate(secret):
+        for j in value:
+            answer[i] += j
     return answer
 
 
 print(solution(5, [9, 20, 28, 18, 11], [30, 1, 21, 17, 28]))
+
+def solution(n, arr1, arr2):
+    answer = []
+    for i,j in zip(arr1,arr2):
+        a12 = str(bin(i|j)[2:])
+        a12=a12.rjust(n,'0')
+        a12=a12.replace('1','#')
+        a12=a12.replace('0',' ')
+        answer.append(a12)
+    return answer
