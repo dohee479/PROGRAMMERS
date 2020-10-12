4a1a913a76b40f5d94c8950cc65a9cc4bb0d9f2b
dohee479 <dohee479@naver.com>
2020-10-12 22:58:07 +0900

    프로그래머스

--- Level 1/다트게임.py
+++ Level 1/다트게임.py
@@ -1,24 +1,37 @@
 def solution(dartResult):
     answer = 0
     stack = []
-    for i in reversed(dartResult):
+    for i in dartResult:
         if i == 'S':
-            if stack[-1] == '*':
-                stack.append(1)
+            stack.append(stack.pop() ** 1)
         elif i == 'D':
-            stack.append(2)
+            stack.append(stack.pop() ** 2)
         elif i == 'T':
-            stack.append(3)
+            stack.append(stack.pop() ** 3)
         elif i == '*':
-            stack.append('*')
+            if len(stack) == 1:
+                stack.append(stack.pop() * 2)
+            else:
+                stack.append(stack.pop(-2) * 2)
+                stack.append(stack.pop(-2) * 2)
         elif i == '#':
-            stack.append(-1)
+            stack.append(stack.pop() * -1)
         else:
-            if '*' in stack:
-                stack.append()
-            answer += int(i) ** stack.pop()
+            if i == '0':
+                if stack:
+                    if stack[-1] == 1:
+                        stack.pop()
+                        stack.append(10)
+                    else:
+                        stack.append(0)
+                else:
+                    stack.append(0)
+            else:
+                stack.append(int(i))
+    for i in stack:
+        answer += i
 
     return answer
 
 
-print(solution('1S2D*3T'))
+print(solution("1D2S#10S"))
