2f5c07061438e3b1f241dd26fa2a83ed31db88f7
dohee479 <dohee479@naver.com>
2020-10-13 01:20:50 +0900

    프로그래머스

--- Level 1/다트게임.py
+++ Level 1/다트게임.py
@@ -35,3 +35,19 @@ def solution(dartResult):
 
 
 print(solution("1D2S#10S"))
+
+import re
+
+
+def solution(dartResult):
+    bonus = {'S' : 1, 'D' : 2, 'T' : 3}
+    option = {'' : 1, '*' : 2, '#' : -1}
+    p = re.compile('(\d+)([SDT])([*#]?)')
+    dart = p.findall(dartResult)
+    for i in range(len(dart)):
+        if dart[i][2] == '*' and i > 0:
+            dart[i-1] *= 2
+        dart[i] = int(dart[i][0]) ** bonus[dart[i][1]] * option[dart[i][2]]
+
+    answer = sum(dart)
+    return answer
