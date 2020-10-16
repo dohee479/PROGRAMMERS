71f9698a529ddc10793c0de0e03307fe8367c67c
dohee479 <dohee479@naver.com>
2020-10-16 17:02:49 +0900

    프로그래머스

--- Level 1/키패드 누르기.py
+++ Level 1/키패드 누르기.py
@@ -10,17 +10,30 @@ def solution(numbers, hand):
             answer += 'R'
             right = i - 2
         else:
-            if abs(i - left) == abs(i - right):
-                if hand == "left":
+            l_dis = abs(i - left)
+            r_dis = abs(i - right)
+            if l_dis == 3:
+                l_dis = 1
+            if l_dis == 6:
+                l_dis = 2
+            if r_dis == 3:
+                r_dis = 1
+            if r_dis == 3:
+                r_dis = 2
+            if l_dis == r_dis:
+                if hand == 'left':
                     answer += 'L'
                     left = i
                 else:
                     answer += 'R'
-                    left = i
-            elif
-
-
+                    right = i
+            elif l_dis > r_dis:
+                answer += 'R'
+                right = i
+            else:
+                answer += 'L'
+                left = i
     return answer
 
 
-print(solution([1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5], "right"))
+print(solution([7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2], "left"))
