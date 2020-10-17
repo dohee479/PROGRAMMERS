b1d63d8417de430a75aa15a550b8a52ed66325fb
dohee479 <dohee479@naver.com>
2020-10-17 16:21:11 +0900

    프로그래머스

--- Level 1/키패드 누르기.py
+++ Level 1/키패드 누르기.py
@@ -1,39 +1,33 @@
 def solution(numbers, hand):
     answer = ''
-    left = 0
-    right = 0
+    left = 10
+    right = 12
     for i in numbers:
         if i == 1 or i == 4 or i == 7:
             answer += 'L'
             left = i
         elif i == 3 or i == 6 or i == 9:
             answer += 'R'
-            right = i - 2
+            right = i
         else:
+            if i == 0:
+                i = 11
             l_dis = abs(i - left)
             r_dis = abs(i - right)
-            if l_dis == 3:
-                l_dis = 1
-            if l_dis == 6:
-                l_dis = 2
-            if r_dis == 3:
-                r_dis = 1
-            if r_dis == 3:
-                r_dis = 2
-            if l_dis == r_dis:
+            if sum(divmod(l_dis, 3)) > sum(divmod(r_dis, 3)):
+                answer += 'R'
+                right = i
+            elif sum(divmod(l_dis, 3)) < sum(divmod(r_dis, 3)):
+                answer += 'L'
+                left = i
+            else:
                 if hand == 'left':
                     answer += 'L'
                     left = i
                 else:
                     answer += 'R'
                     right = i
-            elif l_dis > r_dis:
-                answer += 'R'
-                right = i
-            else:
-                answer += 'L'
-                left = i
     return answer
 
 
-print(solution([7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2], "left"))
+print(solution([1, 2, 3, 4, 5, 6, 7, 8, 9, 0], "right"))
