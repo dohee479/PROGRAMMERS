fb25a2b418e496854b6ab620d19ddffb8b016b77
dohee479 <dohee479@naver.com>
2020-11-14 21:25:27 +0900

    프로그래머스

--- Level 2/기능개발.py
+++ Level 2/기능개발.py
@@ -3,8 +3,21 @@ def solution(progresses, speeds):
     stack = []
     for work, speed in zip(progresses, speeds):
         stack.append(round((100 - work) / speed + 0.49))
-    print(stack)
+    compare = 0
+    cnt = 1
+    for i, value in enumerate(stack):
+        if i:
+            if value < compare:
+                cnt += 1
+            else:
+                compare = value
+                answer.append(cnt)
+                cnt = 1
+        else:
+            compare = value
+    answer.append(cnt)
     return answer
 
 
-print(solution([93, 30, 55], [1, 30, 5]))
+print(solution([93, 30, 55, 60, 40, 65],  [1, 30, 5 , 10, 60, 7]))
+print(solution([95, 90, 99, 99, 80, 99], [1, 1, 1, 1, 1, 1]	))
