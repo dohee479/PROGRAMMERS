79e12332797981456caf6d7c8c6fc544dcb99813
dohee479 <dohee479@naver.com>
2020-11-21 19:15:49 +0900

    프로그래머스

--- Level 2/가장 큰 수.py
+++ Level 2/가장 큰 수.py
@@ -1,10 +1,24 @@
+# def solution(numbers):
+#     numbers = list(map(str, numbers))
+#     numbers = sorted(numbers, key=lambda x: x*3, reverse=True)
+#     return str(int(''.join(numbers)))
+
+
+import functools
+
+def comparator(a,b):
+    t1 = a+b
+    t2 = b+a
+    return (int(t1) > int(t2)) - (int(t1) < int(t2)) #  t1이 크다면 1  // t2가 크다면 -1  //  같으면 0
+
 def solution(numbers):
-    numbers = list(map(str, numbers))
-    numbers = sorted(numbers, key=lambda x: x*3, reverse=True)
-    return str(int(''.join(numbers)))
+    n = [str(x) for x in numbers]
+    n = sorted(n, key=functools.cmp_to_key(comparator),reverse=True)
+    answer = str(int(''.join(n)))
+    return answer
 
 
-print(solution([0, 0, 0]))
+print(solution([6, 10, 2]))
 print(solution([3, 30, 34, 5, 9]))
 
 
