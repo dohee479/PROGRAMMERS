32b81cb6d39abd56af927fb79867dbbc3f8d662f
dohee479 <dohee479@naver.com>
2020-12-14 17:03:50 +0900

    프로그래머스

--- Level 2/H-Index.py
+++ Level 2/H-Index.py
@@ -8,3 +8,9 @@ def solution(citations):
 
 print(solution([3, 0, 6, 1, 5]))
 print(solution([10,11,12,13]))
+
+
+def solution(citations):
+    citations.sort(reverse=True)
+    answer = max(map(min, enumerate(citations, start=1)))
+    return answer
