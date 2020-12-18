1f6b4e66a1a839622e0d11a8c51b2f924bf5cef6
dohee479 <dohee479@naver.com>
2020-12-18 15:40:03 +0900

    프로그래머스

--- Level 2/구명보트.py
+++ Level 2/구명보트.py
@@ -1,22 +1,16 @@
 def solution(people, limit):
     answer = 0
     people.sort()
-    for i in range(len(people)):
-        if i:
-            if i != len(people) - 1:
-                if people[i - 1] + people[i] <= limit:
-                    continue
-                if people[i] + people[i + 1] <= limit:
-                    answer += 1
-                else:
-                    answer += 1
-            else:
-                if people[i - 1] + people[i] > limit:
-                    answer += 1
-        else:
-            answer += 1
+    i = 0
+    j = len(people) - 1
+    while i <= j:
+        answer += 1
+        if people[i] + people[j] <= limit:
+            i += 1
+        j -= 1
     return answer
 
 
 print(solution([70, 50, 80, 50], 100))
 print(solution([70, 80, 50], 100))
+
