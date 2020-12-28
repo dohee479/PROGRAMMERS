3ede831a47d83b2d34212d14de24732400a1afd6
dohee479 <dohee479@naver.com>
2020-12-28 22:45:19 +0900

    프로그래머스

--- Level 2/JadenCase 문자열 만들기.py
+++ Level 2/JadenCase 문자열 만들기.py
@@ -1,8 +1,11 @@
 def solution(s):
     answer = ''
+    # 공백 뒤에 시작하는 문자만 대문자이면 된다.
     for i, word in enumerate(s):
+        # 따라서, 맨 앞의 문자와 현재 위치 전의 문자가 공백이면 대문자로 변환했다.
         if i == 0 or s[i - 1] == ' ':
             answer += word.upper()
+        # 그 외에는, 소문자로 변환했다.
         else:
             answer += word.lower()
     return answer
