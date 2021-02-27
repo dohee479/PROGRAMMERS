c647edac566c4b91cea2b54e78eca68f43596756
dohee479 <dohee3959@gmail.com>
2021-02-27 20:07:43 +0900

    프로그래머스

--- Level 1/신규 아이디 추천.py
+++ Level 1/신규 아이디 추천.py
@@ -2,14 +2,27 @@ import re
 
 
 def solution(new_id):
-    answer = ''
-    special = ["!", "#", "@", "*"]
     new_id = new_id.lower()
-    for i in special:
-        new_id = new_id.replace(i, "")
+    p = re.compile('[a-z0-9_.]|-')
+    new_id = "".join(p.findall(new_id))
     # 이부분을 정규표현식으로 처리해보자
-    print(new_id)
-    return answer
+    new_id = re.sub('[.]+', '.', new_id)
+    if new_id[0] == ".":
+        new_id = new_id[1:]
+    if new_id and new_id[-1] == ".":
+        new_id = new_id[:len(new_id) - 1]
+    if not new_id:
+        new_id = "a"
+    new_id = new_id[:15]
+    if new_id[-1] == ".":
+        new_id = new_id[:len(new_id) - 1]
+    if len(new_id) < 3:
+        new_id += new_id[-1] * (3 - len(new_id))
+    return new_id
 
 
-print(solution("...!@BaT#*..y.abcdefghijklm"))
+# print(solution("...!@BaT#*..y.abcdefghijklm"))
+# print(solution("z-+.^."))
+print(solution("=.="))
+print(solution("123_.def"))
+print(solution("abcdefghijklmn.p"))
