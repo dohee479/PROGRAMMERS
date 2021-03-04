c00f6cec536f8085cfd758b02da8a2e698159df5
dohee479 <dohee3959@gmail.com>
2021-03-04 23:51:52 +0900

    프로그래머스

--- Level 2/N개의 최소공배수.py
+++ Level 2/N개의 최소공배수.py
@@ -4,17 +4,17 @@ def solution(arr):
 
     # 유클리드 호제법 이해하자.
     def euclid(n1, n2):
-        while n2:
-            n1, n2 = n2 % n1, n2
-        return n1
+        while n1:
+            n1, n2 = n2 % n1, n1
+        return n2
 
     gcd = arr[0]
     for i in arr:
         gcd = euclid(gcd, i)
 
-    for i in arr:
-        answer *= (i // gcd)
+    for i in range(1, len(arr)):
+        answer *= (arr[i] // gcd)
     return answer
 
 
-print(solution([19332, 78696]))
+print(solution([3, 7, 10]))
