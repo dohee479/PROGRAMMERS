b6c105720ddbd09f4b027bbc6d974f6afe4339c0
dohee479 <dohee3959@gmail.com>
2021-03-06 17:45:52 +0900

    프로그래머스

--- Level 2/N개의 최소공배수.py
+++ Level 2/N개의 최소공배수.py
@@ -1,13 +1,19 @@
-# 유클리드 호제법을 이용하여 풀려했으나 실패
 def solution(arr):
-    answer = 1
-    for i in range(len(arr)):
-        num = arr[i]
-        for j in range(i, len(arr)):
-            if not (arr[j] % num):
-                arr[j] = arr[j] // num
-        answer *= num
-    return answer
+    # 최대 공약수를 구하는 유클리드 호제법
+    # A = Bq + R
+    # A와 B의 최대공약수는 B와 R의 최대공약수와 같다.
+    def gcd(n1, n2):
+        while n1:
+            n1, n2 = n2 % n1, n1
+        return n2
+
+    # 두 수의 최소공배수는 두 수를 곱한후 최대공약수로 나누면 된다.
+    # 두개의 최소공배수를 구하고 그 다음 index의 있는 수와의 최소공배수를 구하는 식
+    lcm = arr[0]
+    for i in arr:
+        lcm = (lcm * i) // gcd(lcm, i)
+
+    return lcm
 
 
-print(solution([3,4,9,16]))
+print(solution([2, 3, 4, 5]))
