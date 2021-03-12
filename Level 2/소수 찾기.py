f156cc3800e6f945223bfa24811cdd323442d375
dohee479 <dohee3959@gmail.com>
2021-03-13 01:49:34 +0900

    프로그래머스

--- Level 2/소수 찾기.py
+++ Level 2/소수 찾기.py
@@ -1,18 +1,6 @@
 def solution(numbers):
     answer = 0
     result = ''
-
-    def backtrack(result):
-        cnt = 0
-        a = len(result)
-        if 1 <= a < len(numbers):
-            cnt += 1
-            return
-
-        for num in numbers:
-            backtrack(result + num)
-
-    backtrack(result)
     return answer
 
 
